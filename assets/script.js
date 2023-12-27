03e545dd73dd1c7e968bd8e7edbd5d7a6bac4b94
WCIII <warrenlathe@me.com>
2023-12-26 14:04:47 -1000

    added first html, js and css files, some notes

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+
+//Use a button, once the user clicks, a question is presented and a timer starts
+
+//The timer starts once button clicked (eventlistener)
+
+// a message appears telling user to answer the questions
+
+// a) Presented with question and multiple choice answers
+//    buttons change/highlight on hover
+// b) If click correct answer, a counter ++ adds to the correct count (goes to local storage)
+// c) if click correct answer an alert button (or html text?) says the answer is correct
+
+// d) else, if incorrect answer, an alert button says answer is wrong and counter ++ adds to wrong count
+
+//once the  question is answered, goes to the next question and repeats a-d
+
+//when timer runs out, quiz over
+//when all questions answered, quiz over
+
+//when quiz is over, score is presented for local storage, initials are requested and shown.
