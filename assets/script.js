e9d2622c84945504ce33cb58e6e78de64ccc2879
WCIII <warrenlathe@me.com>
2023-12-28 17:46:51 -0800

    add draft html, js file (with some variables), empty css file

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,53 @@
+//list variables based on html ids (might not need them all for javascri[tp-])
+// ask if document is needed here:
+let startDisplay = document.getElementById('start-display');
+let startQuiz = document.getElementById('start-quiz');
+let showTimer = document.getElementById('show-timer');
+let timer = document.getElementById('timer');
+let showQuestions = document.getElementById('show-questions');
+let questions = document.getElementById('questions');
+let options = document.querySelectorAll('options');
+let saveScore = document.getElementById('save-score');
+let enterInitials = document.getElementById('enter-initials');
+let saveScoreBtn = document.getElementById('save-scorebtn');
+let showResults = document.getElementById('show-results');
+let showPastResults = document.getElementById('show-past-results');
+let clearResults = document.getElementById('clear-results');
+let pastResultsDiv = document.getElementById('past-results-div');
+let pastResultsList = document.getElementById('past-results-list');
+
+// other variables for within javascript
+let questionList = [
+    {
+        question: '',
+        options: ['', '', '', ''],
+        correctOption: ''
+    },
+    {
+        question: '',
+        options: ['', '', '', ''],
+        correctOption: ''
+    },
+    {
+        question: '',
+        options: ['', '', '', ''],
+        correctOption: ''
+    },
+    {
+        question: '',
+        options: ['', '', '', ''],
+        correctOption: ''
+    },
+    {
+        question: '',
+        options: ['', '', '', ''],
+        correctOption: ''
+    },
+  ];
 
 //Use a button, once the user clicks, a question is presented and a timer starts
 
-//The timer starts once button clicked (eventlistener)
+//The timer starts once button clicked (eventlistener?)
 
 // a message appears telling user to answer the questions
 
